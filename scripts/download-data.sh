c085a87bd16cddddd22a66ca2a98671cf26ca068
Gibran Hemani <explodecomputer@gmail.com>
2025-03-12 21:14:30 +0000

    updated data download script to use config files

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+source config.env
+
+mkdir -p $datadir
+mkdir -p $resultsdir
+
 url="https://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/HNSC/20160128"
 
 # for loop line by line in data/files.csv
@@ -10,7 +15,28 @@ url="https://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/HNSC/20160128
     read
     while IFS=, read -r filename date time size
     do
-        echo "Downloading $filename from $url"
-        curl -s -L $url/$filename
+        if [ ! -f $datadir/$filename ]; then
+            echo "Downloading $filename from $url"
+            curl -L $url/$filename -o $datadir/$filename
+
+            echo "Downloading $filename.md5 from $url"
+            curl -L $url/${filename}.md5 -o $datadir/${filename}.md5
+        fi
     done
-} < files.csv
+} < scripts/files.csv
+
+# Navigate to the data directory
+workdir=$(pwd)
+cd $datadir
+
+# Check md5sums
+{
+    read
+    while IFS=, read -r filename date time size
+    do
+        md5sum -c ${filename}.md5
+    done
+} < $workdir/scripts/files.csv > ${resultsdir}/md5sums.txt
+
+# Navigate back to the original directory
+cd $workdir
